dd8b0c5d223b92cfcbdac066d05e574fa4630a1f
Mustafa Demir <mustafademir013@gmail.com>
2021-10-04 13:13:26 +0300

    refactor Tile Controller

--- Assets/Scripts/Tile/TileComponent.cs
+++ Assets/Scripts/Tile/TileComponent.cs
@@ -4,20 +4,12 @@ using System.Collections.Generic;
 using UnityEngine;
 namespace TileMatch.Tile
 {
-    public class Tile : MonoBehaviour
+    public class TileComponent : MonoBehaviour
     {
-        public static event Action<int, int> TileDestroyed;
-
         private int _row;
         private int _col;
 
         public int Row { get => _row; set => _row = value; }
         public int Col { get => _col; set => _col = value; }
-
-        private void OnMouseDown()
-        {
-            TileDestroyed?.Invoke(Row, Col);
-            Destroy(gameObject);
-        }
     }
 }
--- Assets/Scripts/Tile/TileController.cs
+++ Assets/Scripts/Tile/TileController.cs
@@ -2,25 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using TileMatch.Board;
+using TileMatch.Inp;
 using UnityEngine;
 
 namespace TileMatch.Tile
 {
     public class TileController : MonoBehaviour
     {
+
+        const string TILETAG = "Tile";
+
         [SerializeField] private BoardSettings _boardSettings;
         [SerializeField] private TransformData _transformData;
 
         [SerializeField] private GameObject[] _tilePrefabs;
 
-
         private void OnEnable()
         {
             BoardController.BoardCreated += CreateTiles;
+            InputController.TouchDropped += ClickTile;
         }
         private void OnDisable()
         {
             BoardController.BoardCreated -= CreateTiles;
+            InputController.TouchDropped -= ClickTile;
         }
 
         private void CreateTiles()
@@ -30,8 +35,9 @@ namespace TileMatch.Tile
                 for (int j = 0; j < _boardSettings.ColCount; j++)
                 {
                     Transform tile = SpawnTile(_transformData.SpawnTransforms[i, j]);
-                    tile.GetComponent<Tile>().Row = i;
-                    tile.GetComponent<Tile>().Col = j;
+                    tile.GetComponent<TileComponent>().Row = i;
+                    tile.GetComponent<TileComponent>().Col = j;
+                    tile.parent = transform;
                     TranslateTile(tile, i, j);
                 }
             }
@@ -44,10 +50,18 @@ namespace TileMatch.Tile
             return tr.transform;
         }
 
-        public void TranslateTile(Transform tile, int i, int j)
+        private void TranslateTile(Transform tile, int i, int j)
         {
             tile.DOMove(_transformData.BoardTransforms[i, j].position, 0.5f).SetEase(Ease.OutBounce);
         }
 
+        private void ClickTile(Vector3 touchPos)
+        {
+            Collider2D hit = Physics2D.OverlapPoint(touchPos);
+            if (hit != null && hit.tag == TILETAG)
+            {
+                Destroy(hit.gameObject);
+            }
+        }
     }
 }
